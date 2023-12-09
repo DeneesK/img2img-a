edfff28686df5226ed68718b4dc5641fe4b86fc0
Denis Kalbasov <denis.kalbasov@gmail.com>
2023-12-09 18:08:25 +0300

    ready

--- predict.py
+++ predict.py
@@ -98,7 +98,7 @@ class Predictor(BasePredictor):
         try:
             image = load_image(str(image))
             processor = OpenposeDetector.from_pretrained('lllyasviel/ControlNet')
-            processor2: PidiNetDetector = PidiNetDetector.from_pretrained('lllyasviel/Annotators')
+            processor2: PidiNetDetector = PidiNetDetector.from_pretrained('lllyasviel/Annotators', filename='main/ControlNetHED.pth')
             print('-------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')            
             control_image = processor(image, hand_and_face=True)
             print('-------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
