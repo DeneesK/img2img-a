f64e8040d1390ef02e6aa73a7b9fcd0fd0972889
Denis Kalbasov <denis.kalbasov@gmail.com>
2023-12-11 10:08:50 +0300

    ready

--- watermark.py
+++ watermark.py
@@ -11,13 +11,14 @@ def watermark_with_transparency(input_image_path,
     w_h = int(w_w / 5.9)
     watermark = watermark.resize((w_w, w_h))
     x_watermark = int(width * 0.05)
-    y_watermark = int(height * 0.9)
+    y_watermark = int(height * 0.95) - int(w_h / 2)
     position = (x_watermark, y_watermark)
 
     transparent = Image.new('RGBA', (width, height), (0, 0, 0, 0))
     transparent.paste(base_image, (0, 0))
     transparent.paste(watermark, position, mask=watermark)
     transparent.save(input_image_path)
+    # transparent.show()
 
 
-watermark_with_transparency('output (10).png')
+# watermark_with_transparency('output (10).png')
